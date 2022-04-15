b5e41d6951bce1f841384d9963477aa3b028186a
Tee Wen Seng <wenseng.tee@izeno.com>
2022-04-15 20:02:00 +0800

    type-switchers.go

---
+++ type-switches.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func do(i interface{}) {
+
+	//A type switch is a construct that permits several type assertions in series (can only be used in type)
+	/*
+		A type switch is like a regular switch statement, but the cases in a type switch specify types (not values),
+		and those values are compared against the type of the value held by the given interface value
+	*/
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
+}
+
+func main() {
+	do(21)
+	do("hello")
+	do(true)
+}
